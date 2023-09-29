d54b2b6f6ac3305bb48f284bf52c35845f580705
nadabadr27 <146151537+nadabadr27@users.noreply.github.com>
2023-09-30 01:25:28 +0300

    Update welcome.dart

--- lib/modules/welcomepage/welcome/welcome.dart
+++ lib/modules/welcomepage/welcome/welcome.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smartparking/component/component.dart';
 import 'package:smartparking/constant/constant.dart';
@@ -9,82 +10,86 @@ class welcome extends StatelessWidget {
     return Scaffold(
 
       body:
-    Container(
-      child: Padding(
-        padding: EdgeInsets.only(top: 80),
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-        assetimage(image: 'assets/images/Hello-rafiki.png'),
-        Column(
+    SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Container(
+        child: Padding(
+          padding: EdgeInsets.only(top: 80),
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
+          assetimage(image: 'assets/images/Hello-rafiki.png'),
+          Column(
             children: [
-
-              text(text: 'Welcome',weight: FontWeight.w900,size: 30,color: blue),
-              Image.asset('assets/images/hello.png',width: 45,height: 45,),
-
-            ],
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            text(text: 'Parking Spot',weight: FontWeight.w900,size: 30,color: blue),
-          ],
-        ),
-        SizedBox(
-          height: 10,
-        ),
-          text(text: ' best parking app of the country for',weight: FontWeight.w600,size: 20,color: Colors.black26),
-        SizedBox(
-        height: 10,
-        ),
-          text(text: 'all people in the world',weight: FontWeight.w600,size: 20,color: Colors.black26),
-        SizedBox(
-        width: 50,
-        ),
-          SizedBox(
-            height: 70,
-          ),
-
-        Padding(
-          padding: const EdgeInsets.only(top: 80),
-          child: Container(
-
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Row(
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-              textBottom(text: 'Back', function:(){
-              Navigator.push(context,MaterialPageRoute(builder: (context)=>logo(
-              ),) ,);
-              }),
-                SizedBox(
-                  width: 80,
-                ),
 
-                dot(color: blue),
-                dot(color: Colors.black12),
-                dot(color: blue),
-                dot(color: blue),
-              SizedBox(
-              width: 80,
-              ),
-              matrialbuttom(text: 'Next', width: 80, color: blue, size: 20,colortext: Colors.white, wight: FontWeight.w900,function: (){
-    Navigator.push(context,MaterialPageRoute(builder: (context)=>hi(
-    ),) ,);})
+                text(text: 'Welcome',weight: FontWeight.w900,size: 30,color: blue),
+                Image.asset('assets/images/hello.png',width: 45,height: 45,),
 
               ],
               ),
-            ),
+              SizedBox(
+                width: 10,
+              ),
+              text(text: 'Parking Spot',weight: FontWeight.w900,size: 30,color: blue),
+            ],
           ),
-        )
-        ],
+          SizedBox(
+            height: 10,
+          ),
+            text(text: ' best parking app of the country for',weight: FontWeight.w600,size: 20,color: Colors.black26),
+          SizedBox(
+          height: 10,
+          ),
+            text(text: 'all people in the world',weight: FontWeight.w600,size: 20,color: Colors.black26),
+          SizedBox(
+          width: 50,
+          ),
+            SizedBox(
+              height: 70,
+            ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: Container(
+
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                children: [
+                textBottom(text: 'Back', function:(){
+                Navigator.push(context,MaterialPageRoute(builder: (context)=>logo(
+                ),) ,);
+                }),
+                  SizedBox(
+                    width: 80,
+                  ),
+
+                  dot(color: blue),
+                  dot(color: Colors.black12),
+                  dot(color: blue),
+                  dot(color: blue),
+                SizedBox(
+                width: 80,
+                ),
+                matrialbuttom(text: 'Next', width: 80, color: blue, size: 20,colortext: Colors.white, wight: FontWeight.w900,function: (){
+      Navigator.push(context,MaterialPageRoute(builder: (context)=>hi(
+      ),) ,);})
+
+                ],
+                ),
+              ),
+            ),
+          )
+          ],
+          ),
         ),
       ),
     ) ,
     );
     }
     }
+
